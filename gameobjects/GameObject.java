9e766553cc37bbce0591dbf62ff10d70d0547f4b
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-09-05 23:51:33 +0300

    Displaying a spaceship on the playing field

--- gameobjects/GameObject.java
+++ gameobjects/GameObject.java
@@ -3,6 +3,9 @@ package com.javarush.games.spaceinvaders.gameobjects;
 public class GameObject {
     public double x;
     public double y;
+    public int[][] matrix;
+    public int width;
+    public int height;
 
     public GameObject(double x, double y) {
         this.x = x;

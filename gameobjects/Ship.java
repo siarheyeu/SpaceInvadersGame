65db90224ec7ebe3fb6ff24e10a978992352cd4f
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-09-18 23:50:31 +0300

    Creating a player's ship

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -1,6 +1,8 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
 public class Ship extends GameObject {
+    public boolean isAlive = true;
+
     public Ship(double x, double y) {
         super(x, y);
     }
@@ -10,6 +12,11 @@ public class Ship extends GameObject {
     }
 
     public void setStaticView(int[][] viewFrame) {
+
         setMatrix(viewFrame);
+    }
+
+    public void kill() {
+        isAlive = false;
     }
 }

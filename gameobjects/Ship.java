84eb644882828c03c5359f78af84d4d52c00a731
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-09-22 23:55:37 +0300

    Preparing the matrix for shipwreck

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -2,21 +2,34 @@ package com.javarush.games.spaceinvaders.gameobjects;
 
 public class Ship extends GameObject {
     public boolean isAlive = true;
+    private List<int[][]> frames;
+    private int frameIndex;
 
     public Ship(double x, double y) {
+
         super(x, y);
     }
 
     public Bullet fire() {
+
         return null;
     }
 
     public void setStaticView(int[][] viewFrame) {
-
         setMatrix(viewFrame);
+        frames = new ArrayList<>();
+        frames.add(viewFrame);
+        frameIndex = 0;
     }
 
     public void kill() {
         isAlive = false;
+    }
+
+    public void setAnimatedView(int[][]... viewFrames) {
+        setMatrix(viewFrames[0]);
+
+        frames = Arrays.asList(viewFrames);
+        frameIndex = 0;
     }
 }

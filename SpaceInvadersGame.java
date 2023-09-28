6f6fac1474a91c7bbd1fb175a854c2baeecdad73
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-09-28 23:51:08 +0300

    Realization of the movement of the player's ship

--- SpaceInvadersGame.java
+++ SpaceInvadersGame.java
@@ -108,6 +108,7 @@ public class SpaceInvadersGame extends Game {
     private void moveSpaceObjects() {
 
         enemyFleet.move();
+        playerShip.move();
 
         for (Bullet enemyBullet : enemyBullets) {
             enemyBullet.move();

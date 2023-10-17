9982592ae6896c3edb4d32f5336a4ae22b3bd5dd
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-10-17 23:48:25 +0300

    Implementation of permanent boss animation

--- gameobjects/Boss.java
+++ gameobjects/Boss.java
@@ -8,7 +8,8 @@ public class Boss extends EnemyShip {
 
     public Boss(double x, double y) {
         super(x, y);
-        this.setAnimatedView(ShapeMatrix.BOSS_ANIMATION_FIRST,
+        this.setAnimatedView(true,
+                ShapeMatrix.BOSS_ANIMATION_FIRST,
                 ShapeMatrix.BOSS_ANIMATION_SECOND);
     }
 
